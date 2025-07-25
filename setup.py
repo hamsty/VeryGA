dcdfd7bf6d1e0a49bc870713d224fb1a71c7c959
Tálles de Sousa Costa <tallesdesousacosta@gmail.com>
2025-07-25 17:27:27 -0300

    Fix wrong module name

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from VeryGAPlugin.verygaplugin import VeryGAPlugin
 import os
 
 os.system("git clone https://github.com/hamsty/VeryGA.git /content/VeryGA")
@@ -10,7 +9,7 @@ setup(
     version="1.0",
     author="Tálles de Sousa Costa",
     author_email="tallesdesousacosta@gmail.com",
-    py_modules= [verygaplugin.VeryGAPlugin],
+    py_modules= ["VeryGAPlugin.Verygaplugin"],
     url="https://github.com/hamsty/VeryGA",
     license="LICENSE",
     description="Google Colaboratory plugin to run Verilog VGA Simulation"
